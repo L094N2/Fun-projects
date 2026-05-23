8059b4d92d4d182e9150dd639363230d91ed4189
L094N2 <logancrodden2912@icloud.com>
2026-05-23 21:40:59 +0100

    Fix malformed building array syntax in 3D scene

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { OrbitControls } from '@react-three/drei'
 export const dynamic = 'force-dynamic'
 
 function Scene() {
-  const buildings = Array.from({ length: 16 }, (_, i) => (x
+  const buildings = Array.from({ length: 16 }, (_, i) => ({
     x: (i % 4) * 4 - 6,
     z: Math.floor(i / 4) * 4 - 6,
     h: (i % 4) + 2,
@@ -24,12 +24,12 @@ function Scene() {
         <meshStandardMaterial color="#16213d" />
       </mesh>
 
-       {buildings.map((b, i) => (
-        <mesh key={i} position={[b.x, b.h / 2, b.z]}>
-          <boxGeometry args={[2, b.h, 2]} />
-          <meshStandardMaterial color="slategray" />
-        </mesh>
-      ))}
+       {buildings.map((b, i)) => (
+          <mesh key={i} position={[b.x, b.h / 2, b.z]}>
+            <boxGeometry args={[2, b.h, 2]} />
+            <meshStandardMaterial color="slategray" />
+          </mesh>
+        ))}
 
       <mesh position={[0, 1, 0]}>
         <boxGeometry args={[1, 2, 1]} />
@@ -41,14 +41,14 @@ function Scene() {
   )
 }
 
-const Game = dynamic(() => Promise.resolve(Scene), { ssr: false })
+const Game = dynamic() => Promise.resolve(Scene), { ssr: false })
 
 export default function Page() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <div style={{ position: 'absolute', zIndex: 10, color: 'white', padding: '1rem', fontFamily: 'Arial' }}>
         <h1>Low Poly Heist</h1>
-        <p>3D coo-op heist prototype</p>
+        <p>3D co-op heist prototype</p>
       </div>
       <Game />
     </div>
